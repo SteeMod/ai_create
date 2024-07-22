0b54fc4ba2ecdcc3009661c3b438d3b925411ce3
SteeMod <stephenmodimakwane@gmail.com>
2024-07-22 19:37:26 +0000

    save

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("Medcarealll")
+st.title("Medcareall")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
