bfe5bc4e6913e717b542ed36db3a353da0c60b9f
SteeMod <stephenmodimakwane@gmail.com>
2024-07-23 03:47:18 +0000

    save

--- pages/pages/9_8try2.py
+++ pages/pages/9_8try2.py
@@ -5,7 +5,7 @@ def main():
 
     with st.form("my_form"):
         # Use columns to place fields side by side
-        col1row1, col2row1, col3row1 = st.columns(2)
+        col1row1, col2row1, col1row3 = st.columns(2)
         
         with col1row1:
             first_name = st.text_input("FirstName")
@@ -13,7 +13,7 @@ def main():
         with col2row1:
             last_name = st.text_input("LastName")
         
-        with col1row3
+        with col1row3:
             house_address=st.text_input("HouseAdress")
         # Form submission button
         submitted = st.form_submit_button("Submit")
