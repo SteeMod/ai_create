e15ab182f2d30671435337f0fdc7f1dc7f5e5028
SteeMod <stephenmodimakwane@gmail.com>
2024-07-23 04:34:29 +0000

    SAVE

--- pages/8_scaleup.py
+++ pages/8_scaleup.py
@@ -8,19 +8,20 @@ def main():
         col1, col2 = st.columns(2)
         
         with col1:
-            first_name = st.text_input("FirstName")
+            FirstName = st.text_input("FirstName")
         
         with col2:
-            last_name = st.text_input("LastName")
+            LastName = st.text_input("LastName")
         
-        address = st.text_input("Address")
+        Address = st.text_input("Address")
+        
         col3, col4 = st.columns(2)
          
         with col3:
-            first_name = st.text_input("City")
+            City = st.text_input("City")
         
         with col4:
-            last_name = st.text_input("State")
+            State = st.text_input("State")
             col5, col6 = st.columns(2)
 
         col5, col6 = st.columns(2)
@@ -30,6 +31,7 @@ def main():
         
         with col6:
             Phone = st.text_input("Phone")
+            
             col7, col8 = st.columns(2)
         
         with col7:
@@ -37,10 +39,7 @@ def main():
         
         with col8:
             Allergy2 = st.text_input("Allergy2")
-            col9, col10, col11, col = st.columns()
-
             
-
         # Form submission button
         submitted = st.form_submit_button("Submit")
         if submitted:
