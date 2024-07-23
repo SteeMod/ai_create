cee69441e8ce543ddcef1c291153003e0ff7c91e
SteeMod <stephenmodimakwane@gmail.com>
2024-07-23 06:34:22 +0000

    save

--- pages/pages/10_tabletest.py
+++ pages/pages/10_tabletest.py
@@ -1,13 +1,50 @@
 import streamlit as st
 
 def main():
-    st.title("Form with Table-like Layout")
+    st.title("Verify Accuracy")
 
     with st.form("my_form"):
+        with st.form("my_form"):
+        # Use columns to place fields side by side
+            col1, col2 = st.columns(2)
+        
+        with col1:
+            FirstName = st.text_input("FirstName")
+        
+        with col2:
+            LastName = st.text_input("LastName")
+        
+        Address = st.text_input("Address")
+        
+        col3, col4 = st.columns(2)
+         
+        with col3:
+            City = st.text_input("City")
+        
+        with col4:
+            State = st.text_input("State")
+            col5, col6 = st.columns(2)
+
+        col5, col6 = st.columns(2)
+         
+        with col5:
+            Zipcode = st.text_input("Zipcode")
+        
+        with col6:
+            Phone = st.text_input("Phone")
+            
+            col7, col8 = st.columns(2)
+        
+        with col7:
+            Allergy1 = st.text_input("Allergy1")
+        
+        with col8:
+            Allergy2 = st.text_input("Allergy2")
+
         # Define the number of rows and columns for your table
         num_rows = 4  # Example: 4 rows
         num_columns = 7  
-        column_names = ["Mark X", "Medication", "Dosage", "Frequency", "Form", "Route" "Instructions"]  # Column headers
+        column_names = ["Mark X", "Medication", "Dosage", "Frequency", "Form", "Route", "Instructions"]  # Column headers
 
         # Create a header row for column names
         header_cols = st.columns(num_columns)
