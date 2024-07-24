4122f124f374c3dd95087f3c5bcadc67ac51a8f5
SteeMod <stephenmodimakwane@gmail.com>
2024-07-24 01:27:08 +0000

    save

--- pages/itshalwork.py
+++ pages/itshalwork.py
@@ -1,8 +1,23 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import io
+from azure.storage.blob import BlobServiceClient
 
-# Load the CSV file
-df = pd.read_csv('out1.csv')
+# Azure Blob Storage credentials
+connection_string = 'DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net'
+container_name = 'data1'
+blob_name = 'out1.csv'
+
+# Create a blob client
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+blob_client = blob_service_client.get_blob_client(container_name, blob_name)
+
+# Download the blob data into a stream
+stream = io.BytesIO()
+stream.write(blob_client.download_blob().readall())
+stream.seek(0)
+
+# Load the stream into a DataFrame
+df = pd.read_csv(stream)
 
 # Select columns from 'Day1Yes' to 'Day31Yes'
 selected_columns = df.loc[:, 'Day1Yes':'Day31Yes']
