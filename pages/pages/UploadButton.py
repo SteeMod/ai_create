d0d58324305816a42e6969c770b2671e0839d598
SteeMod <stephenmodimakwane@gmail.com>
2024-07-25 02:49:56 +0000

    victory

--- pages/pages/UploadButton.py
+++ pages/pages/UploadButton.py
@@ -12,15 +12,15 @@ logging.basicConfig(level=logging.INFO)
 
 def main(uploaded_file):
     try:
-        endpoint = os.getenv('FORM_RECOGNIZER_ENDPOINT')
-        credential = AzureKeyCredential(os.getenv('FORM_RECOGNIZER_API_KEY'))
+        endpoint = os.getenv('https://new2two.cognitiveservices.azure.com/')
+        credential = AzureKeyCredential(os.getenv('027ad9245a594c5886cf5d90abecb9d1'))
         client = DocumentAnalysisClient(endpoint, credential)
 
-        model_id = os.getenv('FORM_RECOGNIZER_CUSTOM_MODEL_ID')
+        model_id = os.getenv('Thessa5v6')
 
         # Create BlobServiceClient
-        blob_service_client = BlobServiceClient.from_connection_string(os.getenv('AZURE_STORAGE_CONNECTION_STRING'))
-        container_client = blob_service_client.get_container_client(os.getenv('BLOB_CONTAINER_NAME'))
+        blob_service_client = BlobServiceClient.from_connection_string(os.getenv('DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net'))
+        container_client = blob_service_client.get_container_client(os.getenv('data1'))
 
         # Generate a timestamped filename
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
