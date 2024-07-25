95d9d7e0a839a0bdd31664ed1d570979bc968610
SteeMod <stephenmodimakwane@gmail.com>
2024-07-25 00:45:27 +0000

    save

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("Medcareall")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "At Medrec Easy, we are dedicated to providing exceptional addiction treatment services in a welcoming and inclusive environment. Our team of experienced is committed to helping individuals overcome addiction and achieve long-term. We believe in creating a stigma-free and stereotype-free space where everyone feels comfortable seeking the help they need. Contact today to learn more about our personalized treatment and start your journey towards a healthier, happier life."
 )
