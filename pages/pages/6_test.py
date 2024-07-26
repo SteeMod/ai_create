576fe6ce0d48a2afeb35ded807d3ece15a24d0f7
SteeMod <stephenmodimakwane@gmail.com>
2024-07-26 05:33:55 +0000

    joy

--- pages/pages/6_test.py
+++ pages/pages/6_test.py
@@ -1,3 +1,5 @@
+#This code review and submits 
+
 from azure.storage.blob import BlobServiceClient
 import csv
 import os
@@ -6,48 +8,19 @@ import pandas as pd
 
 # Function to upload CSV data to Azure Blob Storage
 def upload_csv_data_to_blob(df, container_name, blob_name, connection_string):
-    try:
-        # Create a BlobServiceClient object which will be used to create a container client
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-        
-        # Save DataFrame to CSV
-        df.to_csv('temp.csv', index=False)
-        
-        # Upload the created file
-        with open('temp.csv', 'rb') as data:
-            blob_client.upload_blob(data, overwrite=True)
-        
-        # Delete the temporary file
-        os.remove('temp.csv')
-    except Exception as e:
-        st.error(f"Error uploading CSV to blob: {e}")
-
-# Function to download the latest CSV data from Azure Blob Storage
-def download_latest_csv_from_blob(container_name, connection_string):
-    try:
-        # Create a BlobServiceClient object which will be used to create a container client
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        container_client = blob_service_client.get_container_client(container_name)
-        
-        # Get the list of all blobs in the container
-        blob_list = container_client.list_blobs()
-        
-        # Find the latest blob based on the name (assuming the name includes a timestamp or some identifier that increases with each new file)
-        latest_blob_name = max(blob.name for blob in blob_list)
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=latest_blob_name)
-        
-        # Download the blob to a CSV
-        with open('latest.csv', 'wb') as csv_file:
-            csv_file.write(blob_client.download_blob().readall())
-        
-        # Load the CSV data into a DataFrame
-        df = pd.read_csv('latest.csv', on_bad_lines='skip')
-        
-        return df
-    except Exception as e:
-        st.error(f"Error downloading CSV from blob: {e}")
-        return pd.DataFrame()
+    # Create a BlobServiceClient object which will be used to create a container client
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    
+    # Save DataFrame to CSV
+    df.to_csv('temp.csv', index=False)
+    
+    # Upload the created file
+    with open('temp.csv', 'rb') as data:
+        blob_client.upload_blob(data, overwrite=True)
+    
+    # Delete the temporary file
+    os.remove('temp.csv')
 
 # Generate and pre-populate the form based on the CSV data
 def generate_form(df, row_index=0):
@@ -86,13 +59,13 @@ def generate_form(df, row_index=0):
                 'Allergy2': Allergy2
             }
             new_df = pd.DataFrame([new_data])
-            upload_csv_data_to_blob(new_df, 'data1/ReviewedFiles', 'ANODABOSS.csv', 'DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net')
+            upload_csv_data_to_blob(new_df, 'data1/ReviewedFiles', 'ANODABOSS.csv', 'DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net')
 
 # Example usage
 def main():
     st.title("Submit Form Data to Azure Blob Storage")
     # Load your CSV data into a DataFrame
-    df = download_latest_csv_from_blob('data1', 'DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net')
+    df = pd.read_csv('out1.csv')
     generate_form(df)
 
 if __name__ == "__main__":
