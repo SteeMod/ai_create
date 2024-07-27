f36b7363d129d4d4706ebe7ad739278b446af6fb
SteeMod <stephenmodimakwane@gmail.com>
2024-07-27 07:06:15 +0000

    ok

--- pages/pages/Test.py
+++ pages/pages/Test.py
@@ -4,9 +4,9 @@ import streamlit as st
 # Assuming df is your DataFrame
 df = pd.read_csv('latest.csv')
 
-# Split the dataframe into two dataframes at the 5th column index
-df1 = df.iloc[:, :5]
-df2 = df.iloc[:, 5:]
+# Split the dataframe into two dataframes at the 31st to 100th and 101st to 150th column index
+df1 = df.iloc[:, 30:100]  # Python uses 0-based indexing
+df2 = df.iloc[:, 100:150]
 
 # Now you can display these dataframes
 print(df1)
