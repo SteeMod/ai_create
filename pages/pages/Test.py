2855a4b034169b477d343da5176a91078c424dff
SteeMod <stephenmodimakwane@gmail.com>
2024-07-27 10:12:23 +0000

    ok

--- pages/pages/Test.py
+++ pages/pages/Test.py
@@ -6,10 +6,10 @@ import streamlit as st
 df = pd.read_csv('out1.csv')
 
 # Specify the labels of the values you want to select
-labels = ['Day1', 'Day1Yes', 'Day1No', 'Day2', 'Day2Yes', 'Day2No']
+column_names = ['Day1', 'Day1Yes', 'Day1No', 'Day2', 'Day2Yes', 'Day2No']
 
 # Select the rows with these labels
-selected_data = df[df['labels'].isin(labels)]
+selected_data = df[df['column_names'].isin(column_names)]
 
 # Reshape the selected data to have three columns
 # The number of rows will be determined by the number of elements in selected_data
