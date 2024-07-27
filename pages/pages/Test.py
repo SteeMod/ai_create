4ab250e085e15e6610a7c4cfcc564e2da0f88ea2
SteeMod <stephenmodimakwane@gmail.com>
2024-07-27 09:40:48 +0000

    ok

--- pages/pages/Test.py
+++ pages/pages/Test.py
@@ -11,12 +11,21 @@ data = df.values.flatten()
 # Specify the indices of the values you want to select
 indices = [30,31,32,
 37,38,39,
-44,45,46]
+44,45,46,
+51,52,53,
+58,59,60,
+65,66,67,
+72,73,74,
+79,80,81,
+86,87,88,
+93,94,95,
+100,101,102]
+
 
 # Select the values at these indices
 selected_data = data[indices]
 
-# Reshape the selected data to have two columns
+# Reshape the selected data to have three columns
 # The number of rows will be determined by the number of elements in selected_data
 reshaped_data = np.reshape(selected_data, (-1, 3))
 
