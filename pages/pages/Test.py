b471a3b0f6b142256475aa713d7e69e5f4d72daf
SteeMod <stephenmodimakwane@gmail.com>
2024-07-27 10:09:33 +0000

    ok

--- pages/pages/Test.py
+++ pages/pages/Test.py
@@ -5,29 +5,15 @@ import streamlit as st
 # Load the CSV file
 df = pd.read_csv('out1.csv')
 
-# Convert the DataFrame to a 1D array
-data = df.values.flatten()
+# Specify the labels of the values you want to select
+labels = ['Day1', 'Day1Yes', 'Day1No', 'Day2', 'Day2Yes', 'Day2No']
 
-# Specify the indices of the values you want to select
-indices = [30,31,32,
-37,38,39,
-44,45,46,
-51,52,53,
-58,59,60,
-65,66,67,
-72,73,74,
-79,80,81,
-86,87,88,
-93,94,95,
-100,101,102]
-
-
-# Select the values at these indices
-selected_data = data[indices]
+# Select the rows with these labels
+selected_data = df[df['labels'].isin(labels)]
 
 # Reshape the selected data to have three columns
 # The number of rows will be determined by the number of elements in selected_data
-reshaped_data = np.reshape(selected_data, (-1, 3))
+reshaped_data = np.reshape(selected_data.values, (-1, 3))
 
 # Convert the reshaped data back to a DataFrame
 reshaped_df = pd.DataFrame(reshaped_data)
