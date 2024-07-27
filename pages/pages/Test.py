bb4b2b5f1afc24ceca3171b0b135e4edf5c44eb4
SteeMod <stephenmodimakwane@gmail.com>
2024-07-27 10:28:47 +0000

    ok

--- pages/pages/Test.py
+++ pages/pages/Test.py
@@ -1,4 +1,5 @@
 import csv, operator
+import streamlit as st
 
 # Open the CSV file
 with open('out1.csv', 'r') as csvfile:
@@ -7,6 +8,5 @@ with open('out1.csv', 'r') as csvfile:
     # Sort the data by the first column (change the index to sort by a different column)
     sorted_data = sorted(data, key=operator.itemgetter(0))  # Change 0 to the index of the column you want to sort by
 
-# Print each row of the sorted data
-for row in sorted_data:
-    print(row)
+# Display the sorted data on a Streamlit page
+st.write(sorted_data)
