94dfed2cb692b93e32c3c8613d6f09b3b3501f6d
SteeMod <stephenmodimakwane@gmail.com>
2024-07-28 04:41:21 +0000

    ok

---
+++ pages/pages/5_Aboutme.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("About[Me]")
+st.write(
+    "'m Management Information Systems Specialist with over 18 years of experience in the field. I focus on Data Analytics, Data Visualization, and AI Infusion. I have a passion for creating data-driven solutions that help organizations."
+)
