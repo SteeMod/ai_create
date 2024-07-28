392233526e393da2cfee1c06111cd363ac6bc460
SteeMod <stephenmodimakwane@gmail.com>
2024-07-28 23:18:12 +0000

    ok

--- pages/pages/Test.py
+++ pages/pages/Test.py
@@ -11,5 +11,11 @@ for column in column_names:
     if column in data.columns:
         df[column] = data[column]
 
+# Select only columns that contain the word 'Day'
+day_columns = [col for col in df.columns if 'Day' in col]
+
+# Sort these columns and update the DataFrame
+df[day_columns] = df[day_columns].sort_index(axis=1)
+
 # Display the data in a table
 st.table(df)
