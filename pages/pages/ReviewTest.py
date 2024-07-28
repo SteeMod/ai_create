34d7767d4bfc7b412d65b5eef09020bf84beaf6d
SteeMod <stephenmodimakwane@gmail.com>
2024-07-28 21:37:53 +0000

    ok

--- pages/pages/ReviewTest.py
+++ pages/pages/ReviewTest.py
@@ -5,7 +5,7 @@ import pandas as pd
 data = pd.read_csv('out1.csv')
 
 # Select specific columns
-selected_columns = data[["Med1Check, Med1Name, Med1Dosage, Med1Frequency, Med1Form, Med1Route, Med1Instructions" ]]
+selected_columns = data[["Med1Check", "Med1Name", "Med1Dosage", "Med1Frequency", "Med1Form", "Med1Route", "Med1Instructions"]]
 
 # Display the data in a table
 st.table(selected_columns)
