81da5115d927dbbf4168ad7d203c5b1f4306f4b5
SteeMod <stephenmodimakwane@gmail.com>
2024-07-29 19:43:10 +0000

    dasf

--- pages/Home.py
+++ pages/Home.py
@@ -1,8 +1,18 @@
 import streamlit as st
 st.title("Re[Fresh]")
-
-
 st.write ("At Re[Fresh], we are dedicated to providing exceptional addiction treatment services in a welcoming and inclusive environment. Our team of experienced professionals is committed to helping individuals overcome addiction and achieve long-term. We believe in creating a stigma-free and stereotype-free space where everyone feels comfortable seeking the help they need. Contact us today to learn more about our personalized treatment and start your journey towards a healthier, happier life.")
-
+import streamlit as st
+st.title("Instructions")
+st.header("Follow exact instructions without skipping any, otherwise you may recieve innacurracies.")
+st.subheader("Step 1.")
+st.write ("Welcome to ReFresh, this is how the system works, first you go to the download page and either download or print the form you select.")
+st.subheader("Step 2.")
+st.write("Now that you have an editable copy of the form,   fill the form following these guidelines 1. This is a test so do not use any real information use pseudo information to fill the form.")
+st.subheader("Step 3.")
+st.write("After filling the form, Go to the upload page and select your filled form and click submit. If there are no error messages that means it has been uploaded move on to the review page")
+st.subheader("Step 4.")
+st.write("Go to the review Page to check accuracy and edit inaccuracies *Note that innacuracies are caused because the model is still being trained*")
+st.subheader("Final Step")
+st.write("Go to your Dashboard to view your results.")
 
 
