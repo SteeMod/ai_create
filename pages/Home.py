b845846ac6f4e91e9f32df28bb183ffb271eed33
SteeMod <stephenmodimakwane@gmail.com>
2024-07-29 05:31:15 +0000

    ok

--- pages/Home.py
+++ pages/Home.py
@@ -2,8 +2,7 @@ import streamlit as st
 st.title("Re[Fresh]")
 
 
-# Split the text into two parts for the two columns
-text = "At Re[Fresh], we are dedicated to providing exceptional addiction treatment services in a welcoming and inclusive environment. Our team of experienced professionals is committed to helping individuals overcome addiction and achieve long-term. We believe in creating a stigma-free and stereotype-free space where everyone feels comfortable seeking the help they need. Contact us today to learn more about our personalized treatment and start your journey towards a healthier, happier life."
-text_parts = text.split(". ", 2)
+st.write ("At Re[Fresh], we are dedicated to providing exceptional addiction treatment services in a welcoming and inclusive environment. Our team of experienced professionals is committed to helping individuals overcome addiction and achieve long-term. We believe in creating a stigma-free and stereotype-free space where everyone feels comfortable seeking the help they need. Contact us today to learn more about our personalized treatment and start your journey towards a healthier, happier life.")
+
 
 
