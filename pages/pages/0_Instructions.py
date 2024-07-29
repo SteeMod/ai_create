e179d3140cab8c1d1decd3f8c66bfeefd524c3bb
SteeMod <stephenmodimakwane@gmail.com>
2024-07-29 18:35:10 +0000

    0k

--- pages/pages/0_Instructions.py
+++ pages/pages/0_Instructions.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Instructions")
-st.subtile("Step 1")
+st.subtitle("Step 1")
 st.write ("At Re[Fresh], we are dedicated to providing exceptional addiction treatment services in a welcoming and inclusive environment. Our team of experienced professionals is committed to helping individuals overcome addiction and achieve long-term. We believe in creating a stigma-free and stereotype-free space where everyone feels comfortable seeking the help they need. Contact us today to learn more about our personalized treatment and start your journey towards a healthier, happier life.")
 
 
