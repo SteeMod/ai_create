0fd1e7ba7a41ad8f26a70604e15b0df5d67fa304
SteeMod <stephenmodimakwane@gmail.com>
2024-07-29 00:10:33 +0000

    ok

--- pages/pages/Test.py
+++ pages/pages/Test.py
@@ -1,21 +1,18 @@
 import streamlit as st
-import pandas as pd
 
-# Create an empty DataFrame with 7 columns
-column_names = ["Day", "DayYes", "DayNo", "DayDosage", "DayFreq", "Day1Form", "Day1Route"]
-df = pd.DataFrame(columns = column_names)
+# Sidebar
+st.sidebar.title("Menu")
+menu = st.sidebar.radio("Go to", ["Home", "About Us", "Contact Us"])
 
-# Load the CSV data into the DataFrame
-data = pd.read_csv('out1.csv')
-for column in column_names:
-    if column in data.columns:
-        df[column] = data[column]
-
-# Select only columns that contain the word 'Day'
-day_columns = [col for col in df.columns if 'Day' in col]
-
-# Sort these columns and update the DataFrame
-df[day_columns] = df[day_columns].sort_index(axis=1)
-
-# Display the data in a table
-st.table(df)
+# Page contents
+if menu == "Home":
+    st.title("Re[Fresh]")
+    st.write(
+        "At Re[Fresh], we are dedicated to providing exceptional addiction treatment services in a welcoming and inclusive environment. Our team of experienced professionals is committed to helping individuals overcome addiction and achieve long-term. We believe in creating a stigma-free and stereotype-free space where everyone feels comfortable seeking the help they need."
+    )
+elif menu == "About Us":
+    st.title("About Us")
+    # You can add content for the About Us page here
+elif menu == "Contact Us":
+    st.title("Contact Us")
+    # You can add content for the Contact Us page here
