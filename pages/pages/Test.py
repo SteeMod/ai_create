8e9e27e6e8ef315dc6b8c0926cc5f1740482cfb4
SteeMod <stephenmodimakwane@gmail.com>
2024-07-30 20:24:23 +0000

    ok

--- pages/pages/Test.py
+++ pages/pages/Test.py
@@ -1,36 +1,20 @@
 import streamlit as st
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-def append_blob(blob_service_client, container_name, blob_name, content):
-    blob_client = blob_service_client.get_blob_client(container_name, blob_name)
-    
-    # Download the blob to a stream
-    data = blob_client.download_blob().readall()
-    
-    # Decode the blob data
-    decoded_data = data.decode('utf-8')
-    
-    # Append new content
-    updated_data = decoded_data + content + '\n'
-    
-    # Upload the updated blob
-    blob_client.upload_blob(updated_data, overwrite=True)
+# Create a form with a text input in Streamlit
+with st.form(key='my_form'):
+    text_input = st.text_input(label='Enter your comment')
+    submit_button = st.form_submit_button(label='Submit')
 
-def main():
-    # Access secrets from st.secrets
-    connection_string = st.secrets["DEFAULT"]["connection_string"]
-    container_name = st.secrets["DEFAULT"]["azure_container_name"]
+    # If the form is submitted, write the comment to a file
+    if submit_button:
+        with open('comments.txt', 'a') as f:
+            f.write(text_input + '\n')
 
-    blob_name = "comment.csv"
-    
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    
-    st.title('Contact Form')
-    user_input = st.text_area("Please enter your comment: ")
-    
-    if st.button("Send Message"):
-        append_blob(blob_service_client, container_name, blob_name, user_input)
-        st.success("Message sent successfully!")
+        # Create a blob client using the local file name as the name for the blob
+        blob_service_client = BlobServiceClient.from_connection_string('your_connection_string')
+        blob_client = blob_service_client.get_blob_client('your_container_name', 'your_blob_name')
 
-if __name__ == "__main__":
-    main()
+        # Upload the created file
+        with open('comments.txt', 'rb') as data:
+            blob_client.upload_blob(data)
