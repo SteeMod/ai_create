660d3624a997cd7c2c62aed898d9a6c114b9bfaa
SteeMod <stephenmodimakwane@gmail.com>
2024-07-30 07:47:55 +0000

    dar

--- pages/Home.py
+++ pages/Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
-st.title("Re[Fresh]")
-st.write ("At Re[Fresh], we are dedicated to providing exceptional addiction treatment services in a welcoming and inclusive environment. Our team of experienced professionals is committed to helping individuals overcome addiction and achieve long-term. We believe in creating a stigma-free and stereotype-free space where everyone feels comfortable seeking the help they need. Contact us today to learn more about our personalized treatment and start your journey towards a healthier, happier life.")
+st.title("MedicationTracker")
+st.write ("At MedicationTracker, we are dedicated to providing exceptional addiction treatment services in a welcoming and inclusive environment. Our team of experienced professionals is committed to helping individuals overcome addiction and achieve long-term. We believe in creating a stigma-free and stereotype-free space where everyone feels comfortable seeking the help they need. Contact us today to learn more about our personalized treatment and start your journey towards a healthier, happier life.")
 import streamlit as st
 st.title("Instructions")
 st.header("Follow exact instructions without skipping any, otherwise you may recieve innacurracies.")
@@ -14,5 +14,5 @@ st.subheader("Step 4.")
 st.write("Go to the review Page to check accuracy and edit inaccuracies *Note that innacuracies are caused because the model is still being trained*")
 st.subheader("Final Step")
 st.write("Go to your Dashboard to view your results.")
-
+st.write("The latest technology [AI] is making working with the oldest technology[pen and paper] a whole lot easier. The Medication Tracker utilizes Azure AI Document Intelligence to extract medication tracker data from PDF file and save as digital data. I have developed  a simple form to track medication intake by using pen and paper. This is hassle free and more natural way of tracking progress on intake rather than ‘tinkering’ with digital devices which often need WIFI, power, internet, subscription to operate. When its all done you will need one though to upload the PDF :)Please follow instructions to test how well the solution extracts data from raw to digital.  There is a Review stage to check accuracy of the output, particularly hand written recording. Remember to use FICTITIOUS information as this solutions is STRICTLY for testing purposes and is not configured for secure connections and data privacy. Alternatively, you can download an already filled out form and use it for testing.  Please remember to leave some comments on the review section or via email.")
 
