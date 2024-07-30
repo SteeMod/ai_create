9fa24c68abd982e9b9ba2b52b509726b624a3559
SteeMod <stephenmodimakwane@gmail.com>
2024-07-30 00:55:49 +0000

    aad

--- pages/pages/5_Aboutme.py
+++ pages/pages/5_Aboutme.py
@@ -1,17 +1,30 @@
 import streamlit as st
 import streamlit as st
 from PIL import Image
+import streamlit as st
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from PIL import Image
+import io
 st.title("About[Me]")
 st.write(
     "Stephen Modimakwane is a  Management Information Systems Specialist with over 18 years of experience in the field. I focus on Data Analytics, Data Visualization, and AI Infusion. I have a passion for creating data-driven solutions that help organizations."
 )
 
 
-import streamlit as st
-from PIL import Image
 
-# Load the image
-image = Image.open('/workspaces/ai_create/photo.jpg.jpg')
+def show_image():
+    try:
+        blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net")
+        blob_client = blob_service_client.get_blob_client("data1", "photo.png")
 
-# Display the image
-st.image(image, caption='Your Image Caption', use_column_width=True)
+        blob_data = blob_client.download_blob().readall()
+        image_data = Image.open(io.BytesIO(blob_data))
+
+        st.image(image_data, caption='Your Image Caption', use_column_width=True)
+
+    except Exception as ex:
+        print('Exception:')
+        print(ex)
+
+show_image()
+
