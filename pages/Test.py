1de76b653821aa29acc7b047725a0d611ae32a07
SteeMod <stephenmodimakwane@gmail.com>
2024-07-31 20:40:45 +0000

    giga

--- pages/Test.py
+++ pages/Test.py
@@ -4,12 +4,12 @@ pg = st.navigation({
     "Overview": [
         
         # You can also load pages from files, as usual
-        st.Page("0_Instructions.py", title="Instructions", icon=":material/movie_filter::"),
+        st.Page("0_Instructions.py", title="Instructions", icon=":material/movie_filter:"),
 
-        st.Page("1_Download.py", title="Download File", icon=":material/movie_filter::"),
-        st.Page("2_Upload.py", title="Upload", icon=":material/movie_filter::"),
-        st.Page("3_Review,py", title="Review Acurracy", icon=":material/movie_filter::"),
-        st.Page("4_Dashboard.py", title="My Dashboard", icon=":material/movie_filter::"),
+        st.Page("1_Download.py", title="Download File", icon=":material/movie_filter:"),
+        st.Page("2_Upload.py", title="Upload", icon=":material/movie_filter:"),
+        st.Page("3_Review,py", title="Review Acurracy", icon=":material/movie_filter:"),
+        st.Page("4_Dashboard.py", title="My Dashboard", icon=":material/movie_filter:"),
         ],
 })
 pg.run()
