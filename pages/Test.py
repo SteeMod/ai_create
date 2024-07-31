f5c48c71e689f80095637ea3b47270baaa5c7e20
SteeMod <stephenmodimakwane@gmail.com>
2024-07-31 20:38:48 +0000

    fad

--- pages/Test.py
+++ pages/Test.py
@@ -4,10 +4,10 @@ pg = st.navigation({
     "Overview": [
         
         # You can also load pages from files, as usual
-        st.Page("0_Instructions.py", title="Instructions", icon=":🔢:"),
+        st.Page("0_Instructions.py", title="Instructions", icon=":null:"),
 
-        st.Page("1_Download.py", title="Download File", icon=":⬇️:"),
-        st.Page("2_Upload.py", title="Upload", icon=":⬆️:"),
+        st.Page("1_Download.py", title="Download File", icon=":null:"),
+        st.Page("2_Upload.py", title="Upload", icon=":null:"),
         st.Page("3_Review,py", title="Review Acurracy", icon=":null:"),
         st.Page("4_Dashboard.py", title="My Dashboard", icon=":null:"),
         ],
