fab63756a1c326bdd26a6b29ccb82d4e74f8cdb7
SteeMod <stephenmodimakwane@gmail.com>
2024-08-01 03:54:36 +0000

    dcrydtf

--- pages/3_Review.py
+++ pages/3_Review.py
@@ -398,7 +398,7 @@ with st.form("Review"):
 
             # When the 'Submit' button is pressed, the form will be submitted
             submit_button = st.form_submit_button('Submit')
-    if submit_button:
+            if submit_button:
           
                 form_data = pd.DataFrame({
                     'FirstName': [FirstName],
