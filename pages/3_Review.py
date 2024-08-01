2ff220464a919ca076e8cc0d7ab2c83d656b69cf
SteeMod <stephenmodimakwane@gmail.com>
2024-08-01 03:30:16 +0000

    rsd

--- pages/3_Review.py
+++ pages/3_Review.py
@@ -397,6 +397,7 @@ with st.form("Review"):
             PharmacyPhone=PharmacyPhone.text_input("PharmacyPhone", value=str(row_data.get('PharmacyPhone', '')))
 
             # When the 'Submit' button is pressed, the form will be submitted
+            submit_button = st.form_submit_button('Submit')
             if st.form_submit_button('Submit'):
                 form_data = pd.DataFrame({
                     'FirstName': [FirstName],
