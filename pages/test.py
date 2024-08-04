297a27c3e7937382944ca763b9ad925ba079c036
SteeMod <stephenmodimakwane@gmail.com>
2024-08-04 23:28:16 +0000

    praise the lord

--- pages/test.py
+++ pages/test.py
@@ -48,13 +48,7 @@ try:
         mime="application/pdf"
     )
 
-    # Display the PDF using Streamlit's built-in function
-    st.download_button(
-        label="Download PDF",
-        data=file_content,
-        file_name=selected_file,
-        mime="application/pdf"
-    )
+    
 
 except Exception as e:
     st.error(f"An error occurred: {e}")
