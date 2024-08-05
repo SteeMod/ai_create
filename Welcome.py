d3143ed2135fd4c7dcb2a184d28db628ab1b09d5
SteeMod <stephenmodimakwane@gmail.com>
2024-08-05 23:04:52 +0000

    GOD is good

--- Welcome.py
+++ Welcome.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Medication Tracker")
-st.write("Welcome and thank you for testing  this app! 😊")
+st.write("Welcome and thank you for taking time to test this app! 😊")
 st.write("Kindly go to the Home Page, on mobile you should see a symbol like this <, tap on it")
