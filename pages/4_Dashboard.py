c31e4e4d74610a87ace180ac6d3571a39681ff9e
SteeMod <stephenmodimakwane@gmail.com>
2024-08-05 20:04:18 +0000

    drfg

--- pages/4_Dashboard.py
+++ pages/4_Dashboard.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import streamlit as st
-
+import matplotlib.pyplot as plt
 from io import BytesIO
 from azure.storage.blob import BlobServiceClient
 
 st.title("Medication Intake Dashboard")
 
 try:
-    blob_service_client = BlobServiceClient.from_connection_string('DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net')
+    # Hardcoded connection string
+    connection_string = 'DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net'
+    
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     container_client = blob_service_client.get_container_client('data1')
 
     # List all blobs in the virtual folder 'ReviewedFiles'
@@ -20,8 +23,8 @@ try:
     blob_client = blob_service_client.get_blob_client('data1', latest_blob.name)
     df = pd.read_csv(BytesIO(blob_client.download_blob().readall()))
 
-    # Select only columns with names containing 'Day' 'Yes' or 'Day' 'yes'
-    df = df[[col for col in df.columns if 'Day' in col and ('Yes' in col or 'yes' in col)]]
+    # Select only columns with names containing 'Day' and 'Yes' (case-insensitive)
+    df = df[[col for col in df.columns if 'Day' in col and 'Yes' in col]]
 
     # Transpose the DataFrame
     df = df.transpose()
@@ -53,5 +56,4 @@ try:
         st.pyplot(fig)
 
 except Exception as ex:
-    print(f'Exception: {ex}')
- 
+    st.error(f'Exception: {ex}')
