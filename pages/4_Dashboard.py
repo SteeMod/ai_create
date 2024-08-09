f8c5b1b8d0afcdf6d19e85728fa4560e9639868a
SteeMod <stephenmodimakwane@gmail.com>
2024-08-09 23:48:01 +0000

    0k

--- pages/4_Dashboard.py
+++ pages/4_Dashboard.py
@@ -1,29 +1,26 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import matplotlib.pyplot as plt
 from io import BytesIO
 from azure.storage.blob import BlobServiceClient
-import os
 
 st.title("Medication Intake Dashboard")
 
 try:
-    # Azure Blob Storage connection details from environment variables
-    connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-    container_name = 'data1'
-    folder_name = 'ReviewedFiles/'
-
+    # Hardcoded connection string
+    connection_string = 'DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net'
+    
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(container_name)
+    container_client = blob_service_client.get_container_client('data1')
 
     # List all blobs in the virtual folder 'ReviewedFiles'
-    blob_list = container_client.list_blobs(name_starts_with=folder_name)
+    blob_list = container_client.list_blobs(name_starts_with='ReviewedFiles/')
 
     # Find the latest blob
     latest_blob = max(blob_list, key=lambda blob: blob.last_modified)
 
     # Download the latest blob as a pandas DataFrame
-    blob_client = blob_service_client.get_blob_client(container_name, latest_blob.name)
+    blob_client = blob_service_client.get_blob_client('data1', latest_blob.name)
     df = pd.read_csv(BytesIO(blob_client.download_blob().readall()))
 
     # Select only columns with names containing 'Day' and either 'Yes' or 'yes' (case-insensitive)
@@ -44,8 +41,10 @@ try:
     # Count the rows where 'Yes' is either ':selected:' or ':unselected:'
     denominator = df[(df['Yes'] == ':selected:') | (df['Yes'] == ':unselected:')].shape[0]
 
-    # Create a pie chart using Plotly
-    fig = px.pie(values=[numerator, denominator - numerator], names=['Selected', 'Unselected'], title='Your Statistics')
+    # Create a pie chart
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.pie([numerator, denominator - numerator], labels=['Selected', 'Unselected'], autopct='%1.1f%%')
+    ax.set_title('Your Statistics')
 
     # Create two columns
     col1, col2 = st.columns(2)
@@ -54,7 +53,7 @@ try:
     with col1:
         st.dataframe(df)
     with col2:
-        st.plotly_chart(fig)
+        st.pyplot(fig)
 
 except Exception as ex:
     st.error(f'Exception: {ex}')
